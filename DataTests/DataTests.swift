ecc86ca187cf69452cfb7cc63a2af89989441b0f
Rafael Santos <rafael.escaleira@hotmail.com>
2022-06-11 17:14:04 -0400

    ensure RemoteGetEvents calls HttpGetClient with correct URL

--- DataTests/DataTests.swift
+++ DataTests/DataTests.swift
@@ -6,31 +6,39 @@
 //
 
 import XCTest
-@testable import Data
 
-class DataTests: XCTestCase {
+class RemoteGetEvents {
+    private let url: URL
+    private let httpGetClient: HttpGetClient
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    init(url: URL, httpGetClient: HttpGetClient) {
+        self.url = url
+        self.httpGetClient = httpGetClient
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func getEvents() {
+        httpGetClient.get(url: url)
+    }
+}
+
+protocol HttpGetClient {
+    func get(url: URL)
+}
+
+class RemoteGetEventsTests: XCTestCase {
+    func test() {
+        let url = URL(string: "http://any-url.com")!
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteGetEvents(url: url, httpGetClient: httpClientSpy)
+        sut.getEvents()
+        XCTAssertEqual(httpClientSpy.url, url)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    class HttpClientSpy: HttpGetClient {
+        var url: URL?
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        func get(url: URL) {
+            self.url = url
         }
     }
-
 }
