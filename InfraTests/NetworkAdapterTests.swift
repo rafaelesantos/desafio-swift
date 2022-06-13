75e899ac20555742f814ddb5057d00721c94d553
Rafael Santos <rafael.escaleira@hotmail.com>
2022-06-13 00:28:35 -0400

    ensure NetworkAdapter calls URLSessionDataTask with correct URL

--- InfraTests/NetworkAdapterTests.swift
+++ InfraTests/NetworkAdapterTests.swift
@@ -7,7 +7,53 @@
 
 import XCTest
 
-class NetworkAdapterTests: XCTestCase {
-    func test() {
+class NetworkAdapter {
+    private let session: URLSession
+
+    init(session: URLSession = .shared) {
+        self.session = session
     }
+
+    func get(to url: URL) {
+        session.dataTask(with: url).resume()
+    }
+}
+
+class NetworkAdapterTests: XCTestCase {
+    func test_() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = NetworkAdapter(session: session)
+        sut.get(to: url)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+
+    override open class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override open func startLoading() {
+        UrlProtocolStub.emit?(request)
+    }
+
+    override open func stopLoading() {}
 }
