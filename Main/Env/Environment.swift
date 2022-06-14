6cd8eb19259bbd94b56fccb74be7ba9828517db1
Rafael Santos <rafael.escaleira@hotmail.com>
2022-06-14 03:35:29 -0400

    fix env declaration

--- Main/Env/Environment.swift
+++ Main/Env/Environment.swift
@@ -7,15 +7,13 @@
 
 import Foundation
 
-final class Environment {
+public final class Environment {
     public enum EnvironmentVariables: String {
         case apiBaseUrl = "API_BASE_URL"
     }
     
     public static func variable(_ key: EnvironmentVariables) -> String {
-        guard let networkInfoPath = Bundle.main.path(forResource: "NetworkInfo", ofType: "plist") else { fatalError() }
-        guard let networkInfoDict = NSDictionary(contentsOfFile: networkInfoPath) else { fatalError() }
-        guard let value = networkInfoDict[key.rawValue] as? String else { fatalError() }
+        guard let value = Bundle.main.infoDictionary![key.rawValue] as? String else { fatalError() }
         return value
     }
 }
