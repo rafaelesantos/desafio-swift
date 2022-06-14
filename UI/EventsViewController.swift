1e3167035a67437a0d97472201245c610c82efa9
Rafael Santos <rafael.escaleira@hotmail.com>
2022-06-13 23:17:25 -0400

    make EventsViewController implements Loading Protocol

--- UI/EventsViewController.swift
+++ UI/EventsViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import Presentation
 
 final class EventsViewController: UIViewController {
-    let activityIndicatorView: UIActivityIndicatorView = {
-        return UIActivityIndicatorView(style: .large)
+    @UsesAutoLayout
+    var activityIndicatorView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(style: .large)
+        view.hidesWhenStopped = true
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupActivityIndicatorView()
+    }
+    
+    private func setupActivityIndicatorView() {
+        view.addSubview(activityIndicatorView)
+        let constraints = [
+            activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+extension EventsViewController: LoadingProtocol {
+    func display(with model: LoadingModel) {
+        if model.isLoading {
+            activityIndicatorView.startAnimating()
+        } else {
+            activityIndicatorView.stopAnimating()
+        }
     }
 }
---
+++ UI/Helpers/UsesAutoLayout.swift
@@ -0,0 +1,23 @@
+//
+//  UsesAutoLayout.swift
+//  UI
+//
+//  Created by Rafael Escaleira on 13/06/22.
+//
+
+import Foundation
+import UIKit
+
+@propertyWrapper
+public struct UsesAutoLayout<T: UIView> {
+    public var wrappedValue: T {
+        didSet {
+            wrappedValue.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+
+    public init(wrappedValue: T) {
+        self.wrappedValue = wrappedValue
+        wrappedValue.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
