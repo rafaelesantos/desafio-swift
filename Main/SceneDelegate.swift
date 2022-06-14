836d4920122502feee58f36ca97ebb793b1d224b
Rafael Santos <rafael.escaleira@hotmail.com>
2022-06-14 01:30:48 -0400

    add Main layer

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Rafael Escaleira on 14/06/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ MainTests/MainTests.swift
@@ -0,0 +1,15 @@
+//
+//  MainTests.swift
+//  MainTests
+//
+//  Created by Rafael Escaleira on 14/06/22.
+//
+
+import XCTest
+@testable import Main
+
+class MainTests: XCTestCase {
+    func test() {
+        
+    }
+}
